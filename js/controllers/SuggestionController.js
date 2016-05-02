b1cd110277782d22360c09209c4ab2014520d4ab
digitalfreelanceruk <andrewmoore@Andrews-MacBook-Pro.local>
2016-05-02 22:03:16 +0100

    1

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -11,6 +11,6 @@ app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions',
 		$scope.body = '';
 	};
 	$scope.upVote = function(comment) {
-		comment.upvotes += 1;
+		post.comment.upvotes += 1;
 	};
 }]);
