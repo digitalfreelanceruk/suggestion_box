36051f71e232fc41d7c693e1c8b5a73ae8c9aa27
digitalfreelanceruk <andrewmoore@Andrews-MacBook-Pro.local>
2016-05-02 09:04:33 +0100

    1st commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('SuggestionBox', ['ngRoute']);
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		controller: 'HomeController',
+		templateUrl: 'views/home.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
